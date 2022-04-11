c05fd0056df772311a6ad07d0f087a77805fe8f0
flirexchill <wertggg37@mail.ru>
2022-04-11 18:26:48 +0600

    fixed start

--- app/src/main/java/com/flirex/verbum/activities/Start.kt
+++ app/src/main/java/com/flirex/verbum/activities/Start.kt
@@ -3,6 +3,7 @@ package com.flirex.verbum.activities
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import com.flirex.verbum.R
 import com.google.firebase.auth.FirebaseAuth
@@ -11,19 +12,23 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class Start : Activity() {
+    private lateinit var auth: FirebaseAuth
     var tvVerbum: TextView? = null
     var tvBY: TextView? = null
-    private lateinit var auth: FirebaseAuth
+    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
-        tvVerbum = findViewById(R.id.tvVerbum)
-        tvBY = findViewById(R.id.tvBY)
-        val db = Firebase.firestore
-        auth = Firebase.auth
-        val currentUser = auth.currentUser
-        db.collection("users").document(currentUser!!.uid)
-            .get()
+        try {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_start)
+            tvVerbum = findViewById(R.id.tvVerbum)
+            tvBY = findViewById(R.id.tvBY)
+            auth = Firebase.auth
+            val currentUser = auth.currentUser
+            db.collection("users").document(currentUser!!.uid)
+                .get()
+        }catch(e:Exception){
+            Log.d("test", "$e")
+        }
         val intent = Intent(this, LoadingActivity::class.java)
         val handler = android.os.Handler()
         handler.postDelayed({ startActivity(intent) }, 2000)
